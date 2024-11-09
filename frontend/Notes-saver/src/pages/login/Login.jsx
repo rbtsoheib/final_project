6cfc0394d71b5d952fd9cbe17ef1a0680c9ba796
rbtsoheib <taharsoheib08@gmail.com>
2024-11-09 18:20:30 +0100

    new project/ worked on it a little bit and will make changes / it's 11/9/2024 and i will be finishing in one week or 8 days from now, i made the Navbar and made the environment for good work , see you tomorrow

---
+++ frontend/Notes-saver/src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+
+const Login = () => {
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default Login ; 
