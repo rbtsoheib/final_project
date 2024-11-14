ce1970b306e92897ddf0788a4589292a472cc320
rbtsoheib <taharsoheib08@gmail.com>
2024-11-14 12:20:55 +0100

    gg new changes,  can't see the results for some reason, will see what's the problem

--- frontend/Notes-saver/src/components/Cards/ProfileInfo.jsx
+++ frontend/Notes-saver/src/components/Cards/ProfileInfo.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react'; 
+import { getInitials } from '../../utils/helper';
 
-const ProfileInfo = () => {
+const ProfileInfo = ({ onLogout }) => {
   return (
-    <div>
-      hello hehe
+    <div className='flex items-center gap-3'>
+
+      <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-900 font-medium bg-slate-100'>
+        {getInitials("Tahar Soheib")} 
+      </div>
+
+      <div className=''>
+
+        <p className='text-sm font-medium'> Tahar Soheib </p>
+        <button className='text-sm text-slate-700 underline' onClick={onLogout}>
+          Logout
+        </button>
+
+      </div>
+
     </div>
   )
 }
 
-export default ProfileInfo
+export default ProfileInfo ; 
