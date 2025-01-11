e0df42d40117ccef9c226a6f3820f91e49649a0f
rbtsoheib <taharsoheib08@gmail.com>
2025-01-11 19:32:22 +0100

    nnnnnnnnnnnnnn

--- backend/index.js
+++ backend/index.js
@@ -68,7 +68,7 @@ app.post("/create-account", async (req, res) => {
     await user.save();
 
     const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn:"36000m"
+        expiresIn:"36000m",
     });
 
     return res.json({
